99fb62d904671e1285512421a8cbe9b243c2d849
Gregg Derp <cptderp17@gmail.com>
2025-02-12 13:19:47 -0800

    add spring-boot

---
+++ src/test/java/com/example/gagespring/GagespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gagespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
